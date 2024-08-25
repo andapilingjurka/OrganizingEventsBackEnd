635eeb72ce2a9f3033ffe8887cc84dd7add6b6fb
andapilingjurka <ap56170@ubt-uni.net>
2024-08-24 23:18:14 -0700

    Krijimi i klases 'Events'

--- OrganizingEvents/OrganizingEvents/Data/ApplicationDbContext.cs
+++ OrganizingEvents/OrganizingEvents/Data/ApplicationDbContext.cs
@@ -9,9 +9,32 @@ namespace OrganizingEvents.Data
           : base(options)
         { }
 
+        public DbSet<Events> Events { get; set; }
         public DbSet<EventCategories> EventCategories { get; set; }
 
         public DbSet<EventThemes> EventThemes { get; set; }
+
+       
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Events>()
+            .HasOne(p => p.EventThemes)
+            .WithMany()
+            .HasForeignKey(p => p.ThemeId) //Foreign Key
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<Events>()
+           .HasOne(p => p.EventCategories)
+           .WithMany()
+           .HasForeignKey(p => p.CategoryId) //Foreign Key
+           .OnDelete(DeleteBehavior.Restrict);
+
+        }
 
     }
 }
---
+++ OrganizingEvents/OrganizingEvents/Models/Events.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrganizingEvents.Models
+{
+    public class Events
+    {
+        [Key]
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+
+        public int Capacity { get; set; }
+        public string Image { get; set; }
+
+        public string Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public int ThemeId { get; set; }
+        public EventCategories EventCategories { get; set; }
+
+        public EventThemes EventThemes { get; set; }
+
+    }
+}
