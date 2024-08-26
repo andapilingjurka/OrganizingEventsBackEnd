0575a3a9dfd6777f2bf30fe19f851823b72743bc
andapilingjurka <ap56170@ubt-uni.net>
2024-08-26 12:50:15 -0700

    Krijimi i 'EventCategoriesController' metodat GET dhe POST

---
+++ OrganizingEvents/OrganizingEvents/Controllers/EventCategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using OrganizingEvents.Data;
+using OrganizingEvents.Models;
+
+namespace OrganizingEvents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventCategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EventCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //ListAll
+        [HttpGet]
+        [Route("GetAllList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var category = await _db.EventCategories.ToListAsync();
+            return Ok(category);
+        }
+
+
+        //GetById
+        [HttpGet]
+        [Route("GetEventCategoriesById")]
+        public async Task<IActionResult> GetEventCategoriesByIdAsync(int Id)
+        {
+            var category = await _db.EventCategories.FindAsync(Id);
+            return Ok(category);
+        }
+
+
+        //Add
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> PostAsync(EventCategories category)
+        {
+            _db.EventCategories.Add(category);
+            await _db.SaveChangesAsync();
+            return Created($"/GetEventCategoriesById/{category.Id}", category);
+        }
+    }
+}
