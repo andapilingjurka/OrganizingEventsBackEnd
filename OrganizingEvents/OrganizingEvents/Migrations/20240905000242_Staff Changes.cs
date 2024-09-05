eee13ff241ea1e593b9e1dfc6fcdedb93cb6324d
andapilingjurka <ap56170@ubt-uni.net>
2024-09-04 17:03:42 -0700

    Ndryshime

---
+++ OrganizingEvents/OrganizingEvents/Migrations/20240905000242_Staff Changes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrganizingEvents.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaffChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FistName",
+                table: "Staff",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Staff",
+                newName: "FistName");
+        }
+    }
+}
