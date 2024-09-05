23949bcfcbfbc44d0c8a21490cf59fcea497ba51
Shaqir Topko <topkoshaqir@gmail.com>
2024-09-05 23:57:38 +0200

    Krijimi i klases "Feedback"

---
+++ OrganizingEvents/OrganizingEvents/Models/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrganizingEvents.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public int EventsId { get; set; }
+
+        public User User { get; set; }
+
+        public Events Events { get; set; }
+    }
+}
