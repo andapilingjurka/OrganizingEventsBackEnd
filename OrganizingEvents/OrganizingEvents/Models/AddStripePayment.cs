3d7adadb4822fe3b5d7c93b42cf7f99880dc15cb
andapilingjurka <ap56170@ubt-uni.net>
2024-09-20 01:02:08 -0700

    Krijimi i modelit "AddStripePayment"

---
+++ OrganizingEvents/OrganizingEvents/Models/AddStripePayment.cs
@@ -0,0 +1,13 @@
+﻿namespace OrganizingEvents.Models
+{
+    public record AddStripePayment(
+   string CardNumber,
+   string ExpirationYear,
+   string ExpirationMonth,
+   string Cvc,
+   string Email,
+   string Description,
+   string Currency,
+   long Amount
+);
+}
