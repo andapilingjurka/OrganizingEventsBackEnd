a8b13b1976453431bf5a87fc138b361d7f80fa9b
andapilingjurka <ap56170@ubt-uni.net>
2024-09-20 01:01:11 -0700

    Krijimi i modelit "StripePayment"

---
+++ OrganizingEvents/OrganizingEvents/Models/StripePayment.cs
@@ -0,0 +1,13 @@
+﻿namespace OrganizingEvents.Models
+{
+    public record StripePayment(
+      string CardNumber,
+      string ExpirationYear,
+      string ExpirationMonth,
+      string Cvc,
+      string Email,
+      string Description,
+      string Currency,
+      long Amount,
+      string PaymentId);
+}
