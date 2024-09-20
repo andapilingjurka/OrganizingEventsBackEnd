a423af71def5f833c332026802ce72e19d4f92bd
andapilingjurka <ap56170@ubt-uni.net>
2024-09-20 01:07:56 -0700

    Krijimi i folder Contracts dhe file "IStripeAppService"

---
+++ OrganizingEvents/OrganizingEvents/Contracts/IStripeAppService.cs
@@ -0,0 +1,9 @@
+﻿using OrganizingEvents.Models;
+
+namespace OrganizingEvents.Contracts
+{
+    public interface IStripeAppService
+    {
+        Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken ct);
+    }
+}
