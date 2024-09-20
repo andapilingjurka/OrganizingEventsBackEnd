10aef23059b3b5a33c2d53d7b3cfbbce737f5004
andapilingjurka <ap56170@ubt-uni.net>
2024-09-20 01:13:53 -0700

    Krijimi i "StripeController"

---
+++ OrganizingEvents/OrganizingEvents/Controllers/StripeController.cs
@@ -0,0 +1,32 @@
+﻿using OrganizingEvents.Contracts;
+using OrganizingEvents.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrganizingEvents.Controllers
+{
+
+    public class StripeController : Controller
+    {
+        private readonly IStripeAppService _stripeService;
+
+        public StripeController(IStripeAppService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+
+
+        [HttpPost("/api/payment/add")] // Rruga për shtimin e pagesës
+        public async Task<ActionResult<StripePayment>> AddStripePayment(
+    [FromBody] AddStripePayment payment,
+    CancellationToken ct)
+        {
+            StripePayment createdPayment = await _stripeService.AddStripePaymentAsync(
+                payment,
+                ct);
+
+            return StatusCode(StatusCodes.Status200OK, createdPayment);
+        }
+    }
+}
